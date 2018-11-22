6446e5fc1a0ed29749860fd37400e54799e964c1
VPJP <vpjpeltonen@gmail.com>
2018-11-22 16:56:29 +0200

    added json formating

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import time
+import datetime
+import json
 import sound_direction as direct
 
 #GPIO SETUP
@@ -8,11 +10,15 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
 def callback(channel):
+        data = {}
+        date['time'] = datetime.datetime.now()
         if GPIO.input(channel):
-                print "Loud Sound Detected!"
+                data['volume'] = "Loud Sound Detected!"
         else:
-                print "Sound Detected!"
-                direct.tempdirection
+                data['volume'] = "Sound Detected!"
+        data['direction'] = direct.tempdirection()        
+        json_data = json.dumps(data)
+        print(json_data)
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
