86f58379ba4602291c1cd5df98c6131053490144
VPJP <vpjpeltonen@gmail.com>
2018-12-12 14:01:51 +0200

    trying multi pin

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import json
 import sound_direction as direct
 import database_code
 
-#GPIO SETUP
-channel = 4
+#GPIO SETUP, all pins set up
+channel_a = 4
+channel_b = 17
+channel_c = 27
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel, GPIO.IN)
+GPIO.setup(channel_a, GPIO.IN)
+GPIO.setup(channel_b, GPIO.IN)
+GPIO.setup(channel_c, GPIO.IN)
 
 def oldcallback(channel):
         data = {}
@@ -28,8 +32,21 @@ def callback(channel):
         database_code.add_bark(data)      
         print(data)
 
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+def callback_a(channel):
+        print('a noise')
+
+def callback_b(channel):
+        print('b noise')
+
+def callback_c(channel):
+        print('c noise')
+
+GPIO.add_event_detect(channel_a, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_detect(channel_b, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_detect(channel_c, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel_a, callback_a)  # assign function to GPIO PIN, Run function on change
+GPIO.add_event_callback(channel_b, callback_b)  # assign function to GPIO PIN, Run function on change
+GPIO.add_event_callback(channel_c, callback_c)  # assign function to GPIO PIN, Run function on change
 
 # infinite loop
 while True:
